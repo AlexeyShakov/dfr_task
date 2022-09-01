f60129fd075e6557dfe3f1bbceb772d8a79a8716
AlexeyShakov <alexei_96@inbox.ru>
2022-09-01 18:46:58 +0300

    created Client table

--- mailing_service/mailing/admin.py
+++ mailing_service/mailing/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Mailing
+from .models import Mailing, Client
 
 # Register your models here.
 admin.site.register(Mailing)
+admin.site.register(Client)
--- mailing_service/mailing/models.py
+++ mailing_service/mailing/models.py
@@ -1,7 +1,12 @@
+import pytz as pytz
+from django.core.validators import RegexValidator
 from django.db import models
 
 # Create your models here.
+
+
 class Mailing(models.Model):
+
     GENDER = (
         ("M", "Male"),
         ("F", "Female")
@@ -22,3 +27,20 @@ class Mailing(models.Model):
 
     def __str__(self):
         return f"Рассылка от {self.beginning_date}"
+
+
+class Client(models.Model):
+
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+    GENDER = (
+        ("M", "Male"),
+        ("F", "Female")
+            )
+
+    mobile_validator = RegexValidator(regex=r"^7\d{10}$")
+    mobile_number = models.CharField(max_length=11, validators=[mobile_validator])
+    email = models.CharField()
+    name = models.CharField()
+    surname = models.CharField()
+    gender = models.CharField(max_length=1, choices=GENDER, verbose_name="Пол пользователя")
+    timezone = models.CharField(choices=TIMEZONES, default="UTC")
