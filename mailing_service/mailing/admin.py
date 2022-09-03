9daebbce1cd8712e1559a37f06e35ed9714e338c
AlexeyShakov <alexei_96@inbox.ru>
2022-09-03 13:52:59 +0300

    registered models in admin panel

--- mailing_service/mailing/admin.py
+++ mailing_service/mailing/admin.py
@@ -2,6 +2,18 @@ from django.contrib import admin
 from .models import Mailing, Client, Message
 
 # Register your models here.
-admin.site.register(Mailing)
-admin.site.register(Client)
-admin.site.register(Message)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ["beginning_date", "message", "gender_filter", "operator_code_filter", "ending_date"]
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ["mobile_number", "name", "surname", "email", "tag", "timezone"]
+
+
+@admin.register(Message)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ["time_create", "sending_status", "mailing", "client"]
